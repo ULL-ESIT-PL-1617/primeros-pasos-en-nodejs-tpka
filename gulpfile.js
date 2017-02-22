2740385b8701561a3e96505d54b99dedac380d04
Joaquin Sanchiz <alu0100893755@ull.edu.es>
2017-02-22 16:42:39 +0000

    Adding deploy-gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var shell = require('gulp-shell');
@@ -7,12 +6,16 @@ gulp.task('deploy', function() {
       return gulp.src('./txt/**/*')
         .pipe(ghPages());
 });
+
+gulp.task('deploygh', function() {
+        return gulp.src('').pipe(shell(['node ./scripts/deploy-gh-pages']))
+});
 gulp.task('build', function() {
-	return gulp.src('').pipe(shell(['./scripts/losh generate-gitbook']));
+        return gulp.src('').pipe(shell(['gitbook build ./txt ./gh-pages']));
 });
 gulp.task('wiki', function() {
-	return gulp.src('').pipe(shell(['./scripts/losh generate-wiki']));
+        return gulp.src('').pipe(shell(['node ./scripts/generate-wiki.js']));
 });
 gulp.task('serve', function() {
-	return gulp.src('').pipe(shell(['gitbook serve --port 43210 `pwd` gh-pages']));
-}); 
+        return gulp.src('').pipe(shell(['gitbook serve —port 43210 pwd gh-pages']));
+});
