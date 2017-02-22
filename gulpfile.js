45300eafee35bf89ddb207de5113d8921a08f183
Joaquin Sanchiz <alu0100893755@ull.edu.es>
2017-02-22 15:59:34 +0000

    Actualizando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var shell = require('gulp-shell');
- 
 
-gulp.task('build', shell.task( 'gitbook build ./txt'));
-gulp.task('deploygb', shell.task( 'git push gitbook gh-pages'));
-
-gulp.task('deploygh', [ 'build'], function() {
-  return gulp.src('./_book/**/*')
-    .pipe(ghPages());
+gulp.task('deploy', function() {
+      return gulp.src('./txt/**/*')
+        .pipe(ghPages());
 });
+gulp.task('build', function() {
+	return gulp.src('').pipe(shell(['./scripts/losh generate-gitbook']));
+});
+gulp.task('wiki', function() {
+	return gulp.src('').pipe(shell(['./scripts/losh generate-wiki']));
+});
+gulp.task('serve', function() {
+	return gulp.src('').pipe(shell(['gitbook serve --port 43210 `pwd` gh-pages']));
+}); 
