243bb5dab7f97c79fcd2c10e2a424e8c50b4dbc3
alu0100887686 <alu0100887686@ull.edu.es>
2017-02-22 17:21:29 +0000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var shell = require('gulp-shell');
-
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
-var shell = require('gulp-shell');
 var task = require('shell-task');
 var wiki = require('./package.json').repository.wiki;
 
@@ -12,18 +8,12 @@ gulp.task('deploy', function() {
       return gulp.src('./txt/**/*')
         .pipe(ghPages());
 });
-
-gulp.task('build', function() {
-	return gulp.src('').pipe(shell(['./scripts/losh generate-gitbook']));
-});
 gulp.task('wiki', function() {
 	return gulp.src('').pipe(shell(['./scripts/losh generate-wiki']));
 });
 gulp.task('serve', function() {
 	return gulp.src('').pipe(shell(['gitbook serve --port 43210 `pwd` gh-pages']));
 }); 
-
-
 gulp.task('deploygh', function() {
         return gulp.src('').pipe(shell(['node ./scripts/deploy-gh-pages']))
 });
